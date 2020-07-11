a35a6baa44c1f2175fe697194c40e56f8e37b351
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-07-11 21:07:57 +0630

    Setup Sample test with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { shallow, ShallowWrapper} from 'enzyme';
+import { App } from './App';
+describe('App', () => {
+let subject: ShallowWrapper;
+  beforeEach(() => {
+    subject = shallow(<App />);
+  });
+  it('should be div', () => {
+    expect(subject.type()).toBe('div');
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React,{ FC } from 'react';
 
-function App() {
+export const App:FC<{}> = () => {
   return (
     <div className="App">
       App
