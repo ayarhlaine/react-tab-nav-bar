e614e59a7b283e35e5838dc1660ac0d7a1c597c6
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-07-12 22:27:01 +0630

    Update unit test for App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow, ShallowWrapper} from 'enzyme';
+import NavBar from './NavBar/NavBar';
+import Page from './Page/Page';
 import { App } from './App';
 describe('App', () => {
 let subject: ShallowWrapper;
@@ -8,5 +10,15 @@ let subject: ShallowWrapper;
   });
   it('should be div', () => {
     expect(subject.type()).toBe('div');
+  });
+  describe('NavBar', () => {
+    it('should has NavBar', () => {
+      expect(subject.childAt(0).type()).toBe(NavBar);
+    });
+  });
+  describe('Page', () => {
+    it('should has Page', () => {
+      expect(subject.childAt(1).type()).toBe(Page);
+    });
   });
 });
